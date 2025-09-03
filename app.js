236d4f71f6e23e3337ab6f4b13476b4080d2874e
EaeDave <eudaverdgs@gmail.com>
2025-09-03 12:02:47 -0300

    feat(app.js): added delay(s) function

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const { Builder } = require('selenium-webdriver');
 require('dotenv').config();
 
+function delay(s) {
+    return new Promise(resolve => setTimeout(resolve, s * 1000));
+}
+
+
 async function run() {
     const chrome = await new Builder().forBrowser('chrome').build();
     const RUB_IP = process.env.RUB_IP;
 
     try {
         await chrome.get(`http://${RUB_IP}/vue/#/core/local`);
+        await delay(2);
     } finally {
         await chrome.quit();
     }
